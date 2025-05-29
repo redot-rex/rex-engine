f5ad7536e949c715730624caf7d3ef84c8a933cf
DAShoe <dashoe1@gmail.com>
2025-05-28 23:57:14 -0400

    Lots of modules implemented and rookie mistakes corrected.

--- methods.cmake
+++ methods.cmake
@@ -1,39 +1,48 @@
+function(disable_warnings_for_target TARGET)
+	if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
+		target_compile_options(${TARGET} PRIVATE -w)
+	elseif(MSVC)
+		target_compile_options(${TARGET} PRIVATE /W0)
+	endif()
+endfunction()
+
+
 function(generate_copyright_header OUT_FILE)
 	set(COPYRIGHT_HEADER "\
-/**************************************************************************/\n\
-/* ${OUT_FILE} */\n\
-/**************************************************************************/\n\
-/*                         This file is part of:                          */\n\
-/*                             REDOT ENGINE                               */\n\
-/*                        https://redotengine.org                         */\n\
-/**************************************************************************/\n\
-/* Copyright (c) 2024-present Redot Engine contributors                   */\n\
-/*                                                (see REDOT_AUTHORS.md). */\n\
-/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */\n\
-/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */\n\
-/*                                                                        */\n\
-/* Permission is hereby granted, free of charge, to any person obtaining  */\n\
-/* a copy of this software and associated documentation files (the        */\n\
-/* \"Software\"), to deal in the Software without restriction, including    */\n\
-/* without limitation the rights to use, copy, modify, merge, publish,    */\n\
-/* distribute, sublicense, and/or sell copies of the Software, and to     */\n\
-/* permit persons to whom the Software is furnished to do so, subject to  */\n\
-/* the following conditions:                                              */\n\
-/*                                                                        */\n\
-/* The above copyright notice and this permission notice shall be         */\n\
-/* included in all copies or substantial portions of the Software.        */\n\
-/*                                                                        */\n\
-/* THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND,        */\n\
-/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */\n\
-/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */\n\
-/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */\n\
-/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */\n\
-/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */\n\
-/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */\n\
-/**************************************************************************/\n\
-\n\
-/* THIS FILE IS GENERATED. EDITS WILL BE LOST. */\n\
-\n\
+/**************************************************************************/
+/* ${OUT_FILE} */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             REDOT ENGINE                               */
+/*                        https://redotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2024-present Redot Engine contributors                   */
+/*                                                (see REDOT_AUTHORS.md). */
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* \"Software\"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+/* THIS FILE IS GENERATED. EDITS WILL BE LOST. */
+
 " PARENT_SCOPE)
 
 endfunction()
